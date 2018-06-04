96fcd39eecfeff6c6f9c5eaa5bdb25277bc40cf6
Adam Smith <hagansmith@gmail.com>
2018-06-04 14:15:23 -0500

    Add project files.

---
+++ Small-Shop-API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Small_Shop_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Small-Shop-API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Small_Shop_API.WebApiApplication" Language="C#" %>
